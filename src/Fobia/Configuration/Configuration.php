b7593558e558e2694bce80431dc99a355523db73
Dmitriy Tyurin <fobia3d@gmail.com>
2016-03-01 14:37:01 +0300

    add travis

--- src/Fobia/Configuration/Configuration.php
+++ src/Fobia/Configuration/Configuration.php
@@ -7,27 +7,6 @@
  * @link      http://www.slimframework.com
  * @license   http://www.slimframework.com/license
  * @version   2.3.0
- *
- * MIT LICENSE
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 namespace Fobia\Configuration;
 
@@ -38,6 +17,8 @@ use Fobia\Configuration\Interfaces\ConfigurationHandlerInterface;
 /**
  * Конфигурация
  * Использует класс обработчика конфигурации для разбора конфигурационных данных, доступ к которым в виде массива.
+ *
+ * @author  Dmitriy Tyurin
  *
  * @author  John Porter
  * @link    http://www.slimframework.com
@@ -125,7 +106,7 @@ class Configuration implements ConfigurationInterface, \IteratorAggregate
      * Получить значение
      *
      * @param string $key
-     * 
+     *
      * @return mixed
      */
     public function offsetGet($key)
